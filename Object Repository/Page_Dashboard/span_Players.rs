06cde81ae6133ce2b1c2a1ab7e74836c90e94b05
Milena.Trajkovski <Milena.Trajkovski@BTL-MILEN-TRAJK.seavus.biz>
2019-11-30 19:58:38 +0100

    B

---
+++ Object Repository/Page_Dashboard/span_Players.rs
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Players</name>
+   <tag></tag>
+   <elementGuidId>42f167f9-102a-46f2-b16b-3dab7b62e58a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Test Dashboard 2.15.0.13'])[1]/following::span[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>title</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Players</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;js no-flexbox flexbox-legacy canvas canvastext webgl no-touch geolocation postmessage no-websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths&quot;]/body[@class=&quot;page-header-fixed&quot;]/div[@class=&quot;page-container&quot;]/div[@class=&quot;page-sidebar nav-collapse collapse&quot;]/ul[@class=&quot;page-sidebar-menu&quot;]/li[3]/a[1]/span[@class=&quot;title&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Test Dashboard 2.15.0.13'])[1]/following::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='QA 2.15.0.9'])[1]/following::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[1]/preceding::span[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Segments'])[1]/preceding::span[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/ul/li[3]/a/span</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/BO Stage 2.15.0.13/Script1575140263953.groovy
@@ -0,0 +1,44 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://bostage1.btobet.net/')
+
+WebUI.setText(findTestObject('Object Repository/Page_Administration Panel - Player Wizard/input_Please log-in_Username'), 
+    'admin')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Administration Panel - Player Wizard/input_Please log-in_Password'), 
+    'q22IHHYN3UAfYBkD/UQ5UQ==')
+
+WebUI.click(findTestObject('Object Repository/Page_Administration Panel - Player Wizard/input_Please log-in_logon'))
+
+WebUI.click(findTestObject('Object Repository/Page_Dashboard/span_Players'))
+
+WebUI.click(findTestObject('Object Repository/Page_Dashboard/a_Search'))
+
+WebUI.click(findTestObject('Object Repository/Page_Player Search/div_Processing'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Player Search/input_Exclude_table-search'), 'B2BTest4')
+
+WebUI.sendKeys(findTestObject('Object Repository/Page_Player Search/input_Exclude_table-search'), Keys.chord(Keys.ENTER))
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Player Search/a_B2BTest4'), 'B2BTest4')
+
+WebUI.closeBrowser()
+
